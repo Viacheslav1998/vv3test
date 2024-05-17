fc12349b94536df1e296e1570f0e4a7cc9a8f1be
Slavik <amazingSevenfix@gmail.com>
2024-05-17 20:01:00 +0500

    spacefix

--- src/components/menu/mainMenu.vue
+++ src/components/menu/mainMenu.vue
@@ -1,17 +1,22 @@
 <template>
-	<div
-	  class="menu"
-	>
-		
+  <div>
+  <!-- придется пока ссылками делоать ... -->
+    <div class="logo">	
+    </div>
+	<div class="nav">
+	  <ul>
+	    <li><p>Desing</p></li>
+		<li><p>Services</p></li>
+		<li><p>Our Business</p></li>
+		<li><p>How We Help</p></li>
+		<li><p>Take The Tour</p></li>
+		<li><p>Contact</p></li>
+	  </ul>
 	</div>
+  </div>
 </template>
 
 <script>
-	alert('hello everyone');
 </script>
 <style>
-	.menu {
-		background: red;
-		height: 200px;
-	}
 </style>
--- src/components/root/Footer.vue
+++ src/components/root/Footer.vue
@@ -1,17 +1,17 @@
 <template>
   <footer>
   	<div class="bg-orange-300 p-5">
-  		Footer | Подвал
-  		<span class="f text-[9px]">здесь бывает что то интерессное</span>
+  	  Footer | Подвал
+  	  <span class="f text-[9px]">здесь бывает что то интерессное</span>
   	</div>
   </footer>
 </template>
 
 <script>
-    import '../../styles/main.css';
-	export default {
-		name: "Footer"
-	};
+  import '../../styles/main.css';
+  export default {
+	name: "Footer"
+  };
 </script>
 
 <style>
