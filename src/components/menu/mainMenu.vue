310788ead6363465dbd108f29d8fb1284ace6350
Slavik <amazingSevenfix@gmail.com>
2024-05-17 23:34:38 +0500

    ...

--- src/components/menu/mainMenu.vue
+++ src/components/menu/mainMenu.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
-  <!-- придется пока ссылками делоать ... -->
-    <div class="logo">	
+    <!-- придется пока ссылками делоать ... -->
+    <div class="wrapper-menu flex flex-row bg-gray-300">
+  	  <div class="logo p-5 bg-blue-950 basis-1/4 flex flex-row-reverse">	
+        <h2 class="text-2xl font-sans text-orange-300 font-bold">XP-SERIOUS</h2>
+      </div>
+	  <div class="nav">
+	    <ul class="flex flex-row basis-1/2 p-5">
+	      <li class="pl-20"><p class="text-lg">Desing</p></li>
+		  <li class="pl-20"><p class="text-lg">Services</p></li>
+		  <li class="pl-20"><p class="text-lg">Our Business</p></li>
+		  <li class="pl-20"><p class="text-lg">How We Help</p></li>
+		  <li class="pl-20"><p class="text-lg">Take The Tour</p></li>
+		  <li class="pl-20"><p class="text-lg">Contact</p></li>
+	    </ul>
+	  </div>
     </div>
-	<div class="nav">
-	  <ul>
-	    <li><p>Desing</p></li>
-		<li><p>Services</p></li>
-		<li><p>Our Business</p></li>
-		<li><p>How We Help</p></li>
-		<li><p>Take The Tour</p></li>
-		<li><p>Contact</p></li>
-	  </ul>
-	</div>
   </div>
 </template>
 
