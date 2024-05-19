9c45930c2927dc16d1dc4f86cdb9076d1a9280f7
Slavik <amazingSevenfix@gmail.com>
2024-05-19 20:29:29 +0500

    upd

--- src/components/menu/mainMenu.vue
+++ src/components/menu/mainMenu.vue
@@ -4,6 +4,7 @@
     <div class="wrapper-menu flex flex-row bg-gray-300">
   	  <div class="logo p-5 bg-blue-950 basis-1/4 flex flex-row-reverse">	
         <h2 class="text-2xl font-sans text-orange-300 font-bold">XP-SERIOUS</h2>
+        <img src="../../../public/images/p4.png" alt="logo" class="pr-2">
       </div>
 	  <div class="nav">
 	    <ul class="flex flex-row basis-1/2 p-5">
