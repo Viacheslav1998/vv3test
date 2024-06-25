5f99a2ada472c174b93df50453cc984998f43f5e
Viacheslav1998 <amazingSevenfix@gmail.com>
2024-06-25 17:28:14 +0500

    changes files and fix

---
+++ src/components/menu/block.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>блок с картинкой</p> 
+  </div>
+</template>
--- src/components/root/Header.vue
+++ src/components/root/Header.vue
@@ -2,12 +2,14 @@
   <div 
     class="header">
    	<mainMenu />
+    <block />
    	<spaceSlider/>
   </div>
 </template>
 
 <script setup>
   import mainMenu from '../menu/mainMenu.vue';
+  import block from '../menu/block.vue';
   import spaceSlider from '../sliders/spaceSlider.vue';
   /*block content контент какой то будет с доп блоками*/
 </script>
