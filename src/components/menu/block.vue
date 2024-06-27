9917628a7180099ffc33e7f609fd83bad060031b
Viacheslav1998 <amazingSevenfix@gmail.com>
2024-06-28 02:08:23 +0500

    add new

--- src/components/menu/block.vue
+++ src/components/menu/block.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="wp-block flex justify-center p-8 bg-slate-200">
-    <div class="w-[80%] h-[400px] bg-[url('/part2/m11.jpg')] bg-containt flex rounded-lg drop-shadow-md border-4 border-stone-50">
-      <div class="main-block flex">
-        center block 
-        <!-- с начало этот блок сделай стилизуй подгони под другой -->
+    <div class="w-[80%] h-[400px] bg-[url('/part2/m19.jpg')]  flex rounded-lg  drop-shadow-md border-4 border-stone-50">
+      <div class="main-block flex justify-between w-full">
+        <div class="left-block w-3/5 bg-zinc-950 m-10 flex flex-col p-2 bg-opacity-55 rounded-lg">
+          <div class="">
+            <section class="font-mono text-yellow-50">
+              <h1 class="text-6xl">The Big Company Ideas</h1>
+              <h2 class="text-5xl">Useful Technologies</h2>
+              <h3 class="text-4xl">and evolution</h3>
+            </section>
+          </div>
+          <div class="social">
+            <a href="#">
+              <img src="/social/f.png" alt="facebook">
+              <img src="/social/i.png" alt="instagram">
+              <img src="/social/p.png" alt="pinterest">
+              <img src="/social/t.png" alt="telegram">
+            </a>
+          </div>
+        </div>
+        <div class="right-block w-2/5 bg-red-300 m-10">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit aliquid ducimus, magni laboriosam perspiciatis itaque soluta, consequuntur ratione debitis aperiam suscipit, fugiat quasi quibusdam commodi ea rem. Voluptatum, ratione ducimus.</p>
+        </div>
       </div> 
     </div>
   </div>
 </template>
+
+<script setup>
+</script>
+
+<style>
+</style>
