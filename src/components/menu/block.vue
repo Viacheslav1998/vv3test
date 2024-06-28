7b9729bff516f54a0bb2eadcbc3b75ef13cd21ab
Viacheslav1998 <amazingSevenfix@gmail.com>
2024-06-28 23:06:34 +0500

    time array to test

--- src/components/menu/block.vue
+++ src/components/menu/block.vue
@@ -2,20 +2,20 @@
   <div class="wp-block flex justify-center p-8 bg-slate-200">
     <div class="w-[80%] h-[400px] bg-[url('/part2/m19.jpg')]  flex rounded-lg  drop-shadow-md border-4 border-stone-50">
       <div class="main-block flex justify-between w-full">
-        <div class="left-block w-3/5 bg-zinc-950 m-10 flex flex-col p-2 bg-opacity-55 rounded-lg">
-          <div class="">
+        <div class="left-block w-3/5 bg-zinc-950 m-10 flex flex-col justify-around p-2 bg-opacity-55 rounded-lg">
+          <div class="pl-10">
             <section class="font-mono text-yellow-50">
               <h1 class="text-6xl">The Big Company Ideas</h1>
               <h2 class="text-5xl">Useful Technologies</h2>
-              <h3 class="text-4xl">and evolution</h3>
+              <h3 class="text-4xl">And Evolution</h3>
             </section>
           </div>
-          <div class="social">
-            <a href="#">
-              <img src="/social/f.png" alt="facebook">
-              <img src="/social/i.png" alt="instagram">
-              <img src="/social/p.png" alt="pinterest">
-              <img src="/social/t.png" alt="telegram">
+          <div class="social flex pl-10">
+            <a href="#" 
+              class="p-1"
+              v-for="(image, link) in link_social" :key="link"
+            >
+              <img :src="image" :alt="'Social icon ' + (link + 1)" >
             </a>
           </div>
         </div>
@@ -28,6 +28,12 @@
 </template>
 
 <script setup>
+  const link_social = [
+    "/social/f.png",
+    "/social/i.png",
+    "/social/p.png",
+    "/social/t.png",
+  ]
 </script>
 
 <style>
