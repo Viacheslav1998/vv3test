c817bcc05336b149d733bcc5ad3b9a4416f43b63
Viacheslav1998 <amazingSevenfix@gmail.com>
2024-07-02 04:31:13 +0500

    add slider

--- src/components/sliders/spaceSlider.vue
+++ src/components/sliders/spaceSlider.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>
-    ты готов подключить сюда крутой слайдер ?
+  <div class="wp-custom-slider bg-[url('/part2/m25.jpg')] ">
+    <div class="fone bg-zinc-400 bg-opacity-70 w-full flex justify-end">
+ 
+      <div class="slider-content">
+        вот тут заголовок сделать <br>
+        и выровнять блок 
+        
+      </div>
+      <div class="custom-slider  w-[800px] my-20">
+        <Splide :options="{ rewind: true }">
+          <SplideSlide>
+            <img src="/part2/m11.jpg" alt="Sample 1">
+          </SplideSlide>
+          <SplideSlide>
+            <img src="/part2/m10.jpg" alt="Sample 2">
+          </SplideSlide>
+        </Splide>
+      </div>
+    </div>
   </div>
 </template>
-
 <script setup>
+  import '@splidejs/vue-splide/css/sea-green';
 </script>
 <style>
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 // src/main.js
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueSplide from '@splidejs/vue-splide';
 import './index.css'
 
-createApp(App).mount('#app')
+
+const app = createApp( App );
+app.use( VueSplide );
+app.mount( '#app' );
