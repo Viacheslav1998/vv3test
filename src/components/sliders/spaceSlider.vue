e67bd8a02914cb1b0459ca2faa894e640ddeda9c
Viacheslav1998 <amazingSevenfix@gmail.com>
2024-07-02 15:30:42 +0500

    changes

--- src/components/sliders/spaceSlider.vue
+++ src/components/sliders/spaceSlider.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="wp-custom-slider bg-[url('/part2/m25.jpg')] ">
-    <div class="fone bg-zinc-400 bg-opacity-70 w-full flex justify-end">
+    <div class="fone bg-gray-500 bg-opacity-70 w-full flex justify-around">
  
-      <div class="slider-content">
-        вот тут заголовок сделать <br>
-        и выровнять блок 
-        
+      <div class="custom-content  w-[500px] bg-gray-500 my-20 p-5">
+        <h5 class="text-2xl">The choice is obvious</h5>
+        <p>
+          Huge selection of good projects
+          architects have been coming up with this for years 
+          building these projects also takes into account 
+          moisture. frost resistance. temperature in general.
+          The company was formed long before the reign of the King Count.
+        </p>  
+        <h5 class="text-2xl">Who even knows what this is about?</h5>
+        <p>A historian whose company has long been responsible for the quality and choice of materials that have been carefully selected. or it wouldn't be legendary. </p>
       </div>
-      <div class="custom-slider  w-[800px] my-20">
+
+      <div class="custom-slider w-[500px] my-20">
         <Splide :options="{ rewind: true }">
           <SplideSlide>
             <img src="/part2/m11.jpg" alt="Sample 1">
@@ -17,6 +25,7 @@
           </SplideSlide>
         </Splide>
       </div>
+
     </div>
   </div>
 </template>
