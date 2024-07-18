b16c22fef23c737c4831a23a7931b35b27c60d7b
Viacheslav1998 <amazingSevenfix@gmail.com>
2024-07-19 00:42:07 +0500

    quickfix

--- src/main.js
+++ src/main.js
@@ -4,12 +4,10 @@ import App from './App.vue'
 import VueSplide from '@splidejs/vue-splide';
 import VueScrollTo from 'vue-scrollto';
 import store from './store';
-
 import './index.css'
 
-
-const app = createApp( App );
-app.use( VueSplide );
-app.use( VueScrollTo );
+const app = createApp(App);
+app.use(VueSplide);
+app.use(VueScrollTo);
 app.use(store);
-app.mount( '#app' );
+app.mount('#app');
