2a90ec7240ba216515cd7bfc0254730fae3ffe57
Viacheslav1998 <amazingSevenfix@gmail.com>
2024-07-21 13:58:09 +0500

    feat: about

--- src/components/content/About.vue
+++ src/components/content/About.vue
@@ -5,8 +5,8 @@
       <h4 class="text-5xl text-orange-600">this is where you'll get to know the company.</h4>
       <p class="border-b border-slate-900 pl-1" :class="sizeFont">History, Influence, Success ...</p>
     </div>
-    <div class="mx-auto overflow-auto h-[600px] bg-cover bg-fixed bg-center bg-no-repeat shadow-lg"
-      style="background-image:url('https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp');">
+    <div class="p-[50px] overflow-auto h-[400px] bg-cover bg-fixed bg-center bg-no-repeat shadow-lg"
+      style="background-image:url('/part2/about.jpg');">
       <div class="mt-40">
         <div class="bg-white p-4 sm:p-8">
           <div
@@ -14,39 +14,22 @@
             About our company
           </div>
           <div class="mt-1 text-sm font-medium text-slate-500">
-            Adventure calls, conquer mountain trails.
+            adventures in the designated areas
           </div>
           <p class="mt-4 leading-7 text-slate-500">
-            In the realm of towering mountains, where the air is crisp and
-            the vistas stretch endlessly, one finds solace, exhilaration,
-            and a profound connection with the untamed beauty of the natural
-            world.
+           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates laboriosam reiciendis asperiores sequi animi ipsa, adipisci, provident veniam corrupti modi tenetur id repellendus tempore eos. Id esse possimus doloremque deleniti.
           </p>
           <p class="mt-4 leading-7 text-slate-500">
-            As the sun rises over the rugged peaks, casting a golden glow on
-            the majestic landscape, a sense of awe fills the heart,
-            reminding us of the grandeur and power that reside in the
-            mountains.
-            As the sun rises over the rugged peaks, casting a golden glow on
-            the majestic landscape, a sense of awe fills the heart,
-            reminding us of the grandeur and power that reside in the
-            mountains.
-            As the sun rises over the rugged peaks, casting a golden glow on
-            the majestic landscape, a sense of awe fills the heart,
-            reminding us of the grandeur and power that reside in the
-            mountains.
-            As the sun rises over the rugged peaks, casting a golden glow on
-            the majestic landscape, a sense of awe fills the heart,
-            reminding us of the grandeur and power that reside in the
-            mountains.
-            As the sun rises over the rugged peaks, casting a golden glow on
-            the majestic landscape, a sense of awe fills the heart,
-            reminding us of the grandeur and power that reside in the
-            mountains.
-            As the sun rises over the rugged peaks, casting a golden glow on
-            the majestic landscape, a sense of awe fills the heart,
-            reminding us of the grandeur and power that reside in the
-            mountains.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui, ab hic totam, beatae ea saepe eos, placeat laudantium inventore magni odio suscipit? Minus, optio maiores? Quasi, odio maxime. Tempore, molestiae?
+            Necessitatibus facere at iure ea odit, asperiores quaerat. Quos iste distinctio quis excepturi? Eveniet consequuntur ducimus quo sunt beatae, repudiandae asperiores vero veniam porro, mollitia optio. Enim, sapiente? Molestiae, dolorem.
+            Hic fugiat quos modi repellat rem vitae qui, nam ab facilis blanditiis asperiores veritatis harum incidunt nihil odit necessitatibus dignissimos soluta eos neque illum iusto fugit distinctio magnam. Optio, repellat?
+            Saepe rem facilis repudiandae iure? Ex facilis aut rem ipsam delectus odio soluta, consequatur amet ab laboriosam asperiores, voluptatibus natus. Exercitationem, assumenda tenetur. Eius adipisci officia obcaecati nulla facilis consequatur?
+            Saepe quod beatae nulla consequuntur iste placeat magnam magni aspernatur quis, consectetur dolorum repellat eius. Dolor exercitationem minima repellendus laborum magni ipsa vitae harum dolore, architecto laboriosam sequi assumenda? Soluta.
+            Dolores similique reprehenderit voluptates nemo rem neque quibusdam inventore ad in atque excepturi expedita cupiditate mollitia natus quis hic nobis odit accusantium, fugit voluptatem, saepe est totam recusandae? Nostrum, minus!
+            Porro in ut, iusto ipsum autem ab ullam, distinctio laboriosam eius voluptatum, sint repellendus velit quas illo nam tenetur aut libero quae quam aspernatur deleniti? Atque architecto dolor provident voluptates.
+            Aperiam reiciendis voluptatum sed perspiciatis! A et quo dolores quia atque fuga vitae sunt ea aspernatur, repudiandae quas dolorem inventore esse laudantium veritatis similique aliquid libero quae ipsam asperiores eos!
+            Iure aspernatur ab nostrum expedita labore corrupti perspiciatis eius earum debitis iusto! Ipsa itaque ipsum dolore porro delectus, explicabo tempore repudiandae aperiam nemo ratione optio a. Dolores saepe doloribus repellat!
+            Temporibus veniam perferendis odit? Aliquam error beatae autem aut odio ratione voluptatibus fugit maxime ipsa, inventore temporibus tenetur, repellat unde delectus reiciendis amet tempora quidem dolores exercitationem quae sit quam?
           </p>
         </div>
       </div>
