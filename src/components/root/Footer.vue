433f22af30a5d40d52876b299ffe3db6e96a2ebd
Viacheslav1998 <amazingSevenfix@gmail.com>
2024-07-25 01:11:38 +0500

    feat: footer items . etc

--- src/components/root/Footer.vue
+++ src/components/root/Footer.vue
@@ -2,21 +2,23 @@
   <footer>
 		<div class="w-full bg-gray-700 p-[32px] flex justify-center">
 			<div class="w-[80%] ">
+				
+				<div class="logo flex justify-center my-5 ">
+					<h5 class="text-gray-500 text-xl pb-5">LOGO</h5>
+				</div>
 
-				<div class="logo"><h5>LOGO</h5></div>
-
-				<div class="bg-slate-200 flex">
+				<div class="flex justify-center">
 					<div>
-					  <img src="/short/1.jpg" alt="logo" class="size-32 p-1">
+					  <img src="/short/1.jpg" alt="logo" class="size-32 rounded-lg m-1 shadow-lg">
 					</div>
 					<div>
-					  <img src="/short/2.jpg" alt="logo" class="size-32 p-1">
+					  <img src="/short/2.jpg" alt="logo" class="size-32 rounded-lg m-1 shadow-lg">
 					</div>
 					<div>
-					  <img src="/short/3.jpg" alt="logo" class="size-32 p-1">
+					  <img src="/short/3.jpg" alt="logo" class="size-32 rounded-lg m-1 shadow-lg">
 					</div>
 					<div>
-					  <img src="/short/4.jpg" alt="logo" class="size-32 p-1">
+					  <img src="/short/4.jpg" alt="logo" class="size-32 rounded-lg m-1 shadow-lg">
 					</div>
 				</div>	
 
@@ -31,10 +33,27 @@
 						columb-3
 					</div>
 				</div>
-				<!-- icons here -->
+
+				<div class="logo flex justify-center ">
+					<h5 class="text-gray-500 text-xl pb-5">BEST-PLACE</h5>
+				</div>
+
+				<div class="flex justify-center">
+					<div class="border-t-4 border-lime-400 pt-5">
+					  <img src="/short/5.jpg" alt="logo" class="rounded-lg w-48 m-1 shadow-lg">
+					</div>
+				</div>	
+
+				<div class="">
+SERVICES HERE ....
+				</div>
+
 			</div>
 		</div>
-		<div class="w-full bg-lime-500 h-[20px]"></div>
+
+
+		<div class="w-full bg-lime-500 h-[20px]">
+		</div>
   </footer>
 </template>
 
