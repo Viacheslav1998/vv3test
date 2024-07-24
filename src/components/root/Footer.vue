2916dd2d15d9e175f634f0840454c3603ae4644c
Viacheslav1998 <amazingSevenfix@gmail.com>
2024-07-25 00:53:49 +0500

    feat: footer, style, icons

--- src/components/root/Footer.vue
+++ src/components/root/Footer.vue
@@ -1,15 +1,35 @@
 <template>
   <footer>
-		<div class="w-full h-[500px] bg-gray-700 p-[32px] flex justify-center">
+		<div class="w-full bg-gray-700 p-[32px] flex justify-center">
 			<div class="w-[80%] ">
-				<div class="box-1">
-					columb-1
-				</div>
-				<div class="box-2">
-					columb-2
-				</div>
-				<div class="box-3">
-				  columb-3
+
+				<div class="logo"><h5>LOGO</h5></div>
+
+				<div class="bg-slate-200 flex">
+					<div>
+					  <img src="/short/1.jpg" alt="logo" class="size-32 p-1">
+					</div>
+					<div>
+					  <img src="/short/2.jpg" alt="logo" class="size-32 p-1">
+					</div>
+					<div>
+					  <img src="/short/3.jpg" alt="logo" class="size-32 p-1">
+					</div>
+					<div>
+					  <img src="/short/4.jpg" alt="logo" class="size-32 p-1">
+					</div>
+				</div>	
+
+				<div class="wrapper-boxes">
+					<div class="box-1 ">
+						asd
+					</div>
+					<div class="box-2">
+						columb-2
+					</div>
+					<div class="box-3">
+						columb-3
+					</div>
 				</div>
 				<!-- icons here -->
 			</div>
