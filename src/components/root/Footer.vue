a65877cfe3e7e0e1412cadb33e12eb23d95a13b9
Viacheslav1998 <amazingSevenfix@gmail.com>
2024-07-30 02:25:32 +0500

    feat: footer, contacts

--- src/components/root/Footer.vue
+++ src/components/root/Footer.vue
@@ -23,7 +23,7 @@
 				</div>	
 
 				<div class="logo flex justify-center ">
-					<h5 class="text-gray-500 text-xl p-5">BEST-PLACE</h5>
+					<h5 class="text-gray-500 text-xl p-5">AFFILIATES</h5>
 				</div>
 
 				<div class="flex justify-center">
@@ -38,15 +38,30 @@
 
 				<div class="wrapper-boxes flex justify-center">
 					<div class="box w-[200px] flex flex-col p-5 m-5 text-slate-400 border-t border-slate-500">
-						<h6>addressee: Michigan, Beverly Shores</h6>
-						<p>789789</p>
-						<p>asd</p>
+						<h6 class="text-slate-300 py-2">Сontacts:</h6>
+						<p class="text-xs">Michigan, Beverly Shores</p>
+						<div class="telephone flex py-1">
+							<img class="size-[16px]" src="/images/vc.png" alt="whatsapp">
+							<p class="pl-1 text-xs text-emerald-500">+ 777 36 36 916</p>
+						</div>
+						<p class="text-xs">NYC, Olean</p>
+						<div class="telephone flex py-1">
+							<img class="size-[16px]" src="/images/vc.png" alt="whatsapp">
+							<p class="pl-1 text-xs text-emerald-500">+ 916 88 88 000</p>
+						</div>	
 					</div>
 					<div class="box w-[200px] flex flex-col p-5 m-5 text-slate-400 border-t border-slate-500">
-						columb-2
+						<h6 class="text-slate-300 py-2">Social networks</h6>
+						<div>
+							<Social
+                class="flex justify-center w-[75%]"
+              />
+						</div>
 					</div>
 					<div class="box w-[200px] flex flex-col p-5 m-5 text-slate-400 border-t border-slate-500">
-						columb-3
+						<h6 class="text-slate-300 py-2">Our Developers</h6>
+						<p>Viacheslav.K</p>
+						<p>Yllia.B</p>
 					</div>
 				</div>
 
@@ -63,8 +78,9 @@ SERVICES HERE ....
   </footer>
 </template>
 
-<script>
- 
+<script setup>
+  import Social from '../Social.vue';
+
 </script>
 
 <style>
