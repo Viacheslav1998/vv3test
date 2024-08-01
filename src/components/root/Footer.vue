d557b789c7bd9ead21fd9478e16d718e3519e6a3
Viacheslav1998 <amazingSevenfix@gmail.com>
2024-08-01 22:44:33 +0500

    feat: response mainMenu

--- src/components/root/Footer.vue
+++ src/components/root/Footer.vue
@@ -71,8 +71,8 @@
 
 				<div class="flex justify-center ">
 					<div class="w-[700px] border-t border-slate-500 p-8 flex flex-col">
-						<a href="#" class="w-[100px] text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 my-1">Services</a>
-						<a href="#" class="w-[100px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 my-1">Blog</a>	
+						<a href="#" class="w-[120px] text-xl text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 my-1">Services</a>
+						<a href="#" class="w-[100px] text-xl text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 my-1">Blog</a>	
 						<a v-scroll-to="'#header'" class="w-[24px] py-2 hover:opacity-75">
 							<img src="/images/up2.png" alt="up">
 						</a>
